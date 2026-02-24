0cea78e7fbadee8590c1e23f13a9703b9f379432
shindehiloni-ai <shindehiloni@gmail.com>
2026-02-24 15:56:19 +0530

    day 2 workshop

--- frontend/script.js
+++ frontend/script.js
@@ -16,7 +16,7 @@ event: document.getElementById("event").value
 
 try {
 
-const res = await fetch("http://localhost:5000/register", {
+const res = await fetch("https://gdg-website-u83d.onrender.com/register", {
 method: "POST",
 headers: { "Content-Type": "application/json" },
 body: JSON.stringify(data)
